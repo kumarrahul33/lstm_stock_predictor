a048ee41424d0012cf4f1f57b00c591e4b766c44
Rahul Kumar <rahulkumar@pop-os.localdomain>
2023-11-04 13:11:56 +0530

    add the data_gen file
    - this file will handle all the data cleaning part

---
+++ data_gen.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
+
+
+#  there are simple methods to calculate the technical indicators
+#  for all other columns we have the data in the csv file
+def calculate_macd():
+    # techinical indicator 
+    pass
+
+def calucate_atr():
+    # techinical indicator 
+    pass
+
+def calculate_rsi():
+    # techinical indicator 
+    pass
+
+
+def get_data():
+    pass
